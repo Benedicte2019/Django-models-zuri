1fec97ad6bd1d1748a9fe740ff8efb3a39ea9271
Furaha Benedict <50766789+Benedicte2019@users.noreply.github.com>
2022-06-25 18:50:28 +0200

    updated models.py

--- I4G007705EJR/blog/models.py
+++ I4G007705EJR/blog/models.py
@@ -6,6 +6,6 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     # author = models.ForeignKey(get_user_model()) 
-    author = get_user_model()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_on = models.DateTimeField()
     published_date = models.DateTimeField()
